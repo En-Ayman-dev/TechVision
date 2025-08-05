07761fdbc1ea7da13539fe75406167a9a3a3357a
En-Ayman-dev <a77499n@gmail.com>
2025-08-06 02:47:20 +0300

    وضع روابط ملفات الـ PWA وأيقونات الأجهزة الذكية

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -122,7 +122,7 @@ export default async function LocaleLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="TechVision" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/image/icons/icon-192x192.png" />
         <Script
           id="structured-data-script"
           type="application/ld+json"
